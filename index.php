c3ff27eac1ddf151d16a1c0ed724cea56e4f72fc
joseluisleandro <leandro@blancorama.com>
2017-06-20 20:55:20 -0500

    Prueba

--- index.php
+++ index.php
@@ -1,142 +1,146 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang=''>
 <head>
-<!-- Commit -->
-<title>
-Blancorama
-</title>
+   <title>
+   Blancorama
+   </title>
 
-<meta charset="UTF-8" />
-<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta charset="UTF-8" />
+   <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="css/styles.css">
 
-<link rel="stylesheet" type="text/css" href="css\style.css"/>
+   <link href='imagenes/favicon.png' rel='shortcut icon' type='image/png'>
 
-<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+   <link href='http://fonts.googleapis.com/css?family=Nirmala UI' rel='stylesheet' type='text/css'>
+   <link href='http://fonts.googleapis.com/css?family=Rounded Elegance' rel='stylesheet' type='text/css'>
 
-<style>
-.form{
-  margin:auto;
-  background-color:white;
-  font-size:11px;
-  -webkit-transition: all 0.4s;
-  -moz-transition: all 0.4s;
-  transition: all 0.4s;
-  width:100%;
-  padding:10px;
-  box-sizing:border-box;
-  border:none;
-  color: #ffffff;
-}
-</style>
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="js/script.js"></script>
 
+<!--Comentario-->
 <!--[if IE]-->
-<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7"/>
 <meta http-equiv="imagetoolbar" content="no" />
 <!--[end if]-->
-	
+
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-98442701-1', 'auto');
+  ga('send', 'pageview');
+</script>
+<!-- End Google Analytics -->
+   
 </head>
-
-<body style="margin:0px;">
-	<!--Head-->
-	<header>
-		<article class="container">
-			<div class="row">
-				<div class="logo" style="float:left; padding-left:20px;">
-					<img src="imagenes/logo.jpg" style="width:250px">
-				</div>
-				<div class="facebook" style="float:right; padding-right:50px; padding-top:20px;padding-bottom:10px;">
-					<a href="https://www.blancorama.com/catalog/" style="color:#a42983;font-family:Roboto">Conoce nuestros catálogos</a>
-				</div>
-			</div>
-			<div style="clear:both;">
-			</div>
-			<div class="row" style="height:40px;background-color:#a42983">
-			</div>
-		</article>
-	</header>
-	<!--Head end-->
-
-    <!--Background-->
-    <style type="text/css">
-     @charset "UTF-8";
-
-    .fondo {width: 100%;}
-      @media screen and (max-width: 780px) {.fondo {margin-top:300px;}
-    }
+</head>
+<body>
 
 
-    </style>
 
-	<section>
-		<article class="container">
-			<div class="row">
-				<div class="text">
-					<p style="font-family:Roboto;color:#a42983;font-size:20pt;letter-spacing:2px">PRÓXIMAMENTE<p>
-					<p style="font-family:Roboto;font-size:12pt;">
-					¡Con Blancorama en línea empieza tu propio negocio, 
-<br>o simplemente luce tu hogar en el menor tiempo posible!</p> 
-<p style="font-family:Roboto;color:#a42983;font-size:14pt;">
-<strong>¡Regístrate y entérate de las<strong>
-<br>novedades que tenemos para ti!</p>
+<!--Head-->
+   <header>
+      <article>
+         <div>
+
+            <div class="logo">
+               <img src="imagenes/logo.png" style="width:90%;">
+            </div>
+
+
+            <div id='cssmenu'>
+
+<ul>
+   <li><a href='#'>Inicio</a></li>
+   <li><a href='https://www.blancorama.com/catalog/'>Catálogos</a></li>
+   <li><a href='https://www.facebook.com/Blancorama/'>Contáctanos</a></li>
+</ul>
 </div>
-				<div> <img src="imagenes/fondo-01.jpg" class="fondo" style="background-repeat: no-repeat; background-position:center; width: 100%;"></div>
-				
 
-			    
-			</div>
-		</article>
-	</section>
-	<!--Background end-->
+         <div style="clear:both; background-color:#cecece">
+         </div>
+      </article>
+   </header>
+   <!--Head end-->
 
-    <!--Form-->
-    <style type="text/css">
-     @charset "UTF-8";
-      .table {width: 100%;}
-      td, th, .table {padding: 10px; box-sizing:border-box;}
-      @media screen and (max-width: 650px) {table,tr,td {display: block; width: 120%;}
-    }
-    </style>
 
-	<section>
-		<article class="container">
-			<div class="row" style="height:35%;background-color:#a42983;margin-top:-65px">
-				<table class="table">
-					<tr>
-					<div class="formulario">
-                    <form id="contacto-us" method="post" action="sendmail.php">
-                        <div>
-                            <td><input type="text" name="name" id="name" required="required" class="form" placeholder="Nombre" /></td>
-                            <td><input type="email" name="mail" id="mail" required="required" class="form" placeholder="E-mail" /></td>
-                            <td ><button type="submit" id="submit" name="submit" class="form-btn semibold">ENVIAR</button> 
-                        </div></td>
+
+
+<!--Part 1-->
+   <section>
+      <article>
+         <div>
+            <div class="caja1">
+               <br>
+               <img src="imagenes/vianney.jpg" style="width:100%" class="img"></div> 
+         </div>
+            <div class="caja2">
+             <div class="contenido">
+               <text class="titles">
+               <strong>
+               LUCE TU HOGAR CON ELEGANCIA
+             </strong>
+            </text>
+               <br>
+               <text class="titles2">
+                  Las mejores marcas en un solo lugar
+               </text>
+               <p>
+               <div class="formulario">
+                    <form action="https://facebook.us15.list-manage.com/subscribe/post" method="POST">
+    <input type="hidden" name="u" value="7138a58f3331cbc4ac586fc43">
+    <input type="hidden" name="id" value="9637fff923">
+                            <input type="text" name="MERGE0" id="MERGE0" required="required" class="form" placeholder="Ingresa tu correo para recibir más noticias"/>
+                         <button type="submit" value="Subscribe to list" name="submit" id="mc-embedded-subscribe" class="form-btn semibold">NOTIFICARME</button>
+                        </div>
                         <div class="clear"></div>
                     </form>
-</div>
-</tr>
-</table>
-			</div>
-		</article>
-	</section>
-<!--Form end-->
-<!--Contact-->
-	<section style="height:5%;background-color:#303030;padding:10px;">
-		<article class="container">
-			<div class="row">
-				<div style="float:left;font-family:'Open Sans';color:#ffffff;font-size:9pt;">
-				 Presidente Cárdenas #160, Esq. con Zaragoza Zona Centro. Saltillo, Coahuila. México. C.P. 25000<font color="#303030">.............</font>Tel. (+52) 844 154 52 27
-				</div>
-				<div style="float:right;font-family:'Open Sans';color:#ffffff">
-					<img src="imagenes/facebook.jpg" style="width:20px">
-					<a href="https://www.facebook.com/Blancorama" style="color:#a42983;font-family:Roboto">Blancorama</a>
-				</div>
-			</div>
-			<div style="clear:both;">
-			</div>
-		</article>
-	</section>
-	<!--Contact end-->
-</body>
 
-</html>
+            </div>
+         </div>
+
+         </div>
+      </article>
+   </section>
+   <!--Part1 end-->
+
+   <!--Part 2-->
+   <section class="contenido2">
+      <article>
+         <div class="caja3">
+             <text class="titles">
+               ENCUENTRA EL DISEÑO PERFECTO PARA TU HOGAR
+            </text>
+               <P>
+                  <button class="form-btn semibold"><a href="https://www.blancorama.com/catalog/">VER CATÁLOGOS</a></button>
+            </div>
+            <div>
+               <br>
+               <img src="imagenes/background.jpg" style="width:100%"></div> 
+                  <br>
+                  
+                  <div class="formulario" style="width:60%; display:inline-block;">
+                    <form action="https://facebook.us15.list-manage.com/subscribe/post" method="POST">
+    <input type="hidden" name="u" value="7138a58f3331cbc4ac586fc43">
+    <input type="hidden" name="id" value="9637fff923">
+                            <input type="text" name="MERGE0" id="MERGE0" required="required" class="form" placeholder="Ingresa tu correo para recibir más noticias"/>
+                         <button type="submit" value="Subscribe to list" name="submit" id="mc-embedded-subscribe" class="form-btn semibold">NOTIFICARME</button>
+                        </div>
+                        <div class="clear"></div>
+                    </form>
+
+                    <br>
+                    <br>
+
+         </div>
+      </article>
+   </section>
+   <!--Part 2 end-->
+
+</body>
+<html>
