6e02f6b322de721eebf5678306864052b240d3c5
Mateo <mateo@paybook.com>
2017-08-27 17:23:18 -0500

    Upgrade version

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang=''>
+<html>
 <head>
    <title>
    Blancorama
@@ -8,7 +8,7 @@
    <meta charset="UTF-8" />
    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="stylesheet" href="css/style_0.1.css">
+   <link rel="stylesheet" href="css/style_0.2.css">
 
    <link href='imagenes/favicon.png' rel='shortcut icon' type='image/png'>
 
@@ -17,7 +17,13 @@
    <script src="https://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
    <script src="js/script.js"></script>
 
-<!--Comentario-->
+
+
+  <link rel="stylesheet" type="text/css" media="all" href="style.css">
+  <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+  <script type="text/javascript" charset="utf-8" src="js/jquery.leanModal.min.js"></script>
+
+
 <!--[if IE]-->
 <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7"/>
 <meta http-equiv="imagetoolbar" content="no" />
@@ -34,6 +40,17 @@
   ga('send', 'pageview');
 </script>
 <!-- End Google Analytics -->
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+   <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+   <script>
+      $(document).ready(function()
+      {
+         $("#mostrarmodal").modal("show");
+      });
+    </script>
    
 </head>
 </head>
@@ -66,8 +83,52 @@
    </header>
    <!--Head end-->
 
+<div class="modal fade" id="mostrarmodal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+           <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+              <img src="imagenes/afiliate.jpg" width="80%" style="margin-left:10%"/>
+           </div>
+           
+           <div class="modal-body">
 
+<center>
+<form action="https://blancorama.us15.list-manage.com/subscribe/post" method="POST">
+    <input type="hidden" name="u" value="7138a58f3331cbc4ac586fc43">
+    <input type="hidden" name="id" value="24de628237">
+    
 
+    <!-- people should not fill these in and expect good things -->
+
+    <div id="mergeTable" class="mergeTable">
+        <div class="mergeRow dojoDndItem mergeRow-email" id="mergeRow-0">
+            <label for="MERGE0">E-MAIL <span class="req asterisk">*</span></label>
+            
+                <input type="email" autocapitalize="off" autocorrect="off" name="MERGE0" id="MERGE0" size="25" value="" class="formulario2">
+             
+        </div>
+        <div class="mergeRow dojoDndItem mergeRow-text" id="mergeRow-1">
+            <label for="MERGE1">NOMBRE</label>
+            
+                <input type="text" name="MERGE1" id="MERGE1" size="25" value="" class="formulario2">
+              
+        </div> 
+    </div>
+<br>
+<br>
+        <input type="submit" class="button btn form-btn semibold" name="submit" value="SABER MÁS">
+    <input type="hidden" name="ht" value="3bb178999adba1ab36380e5e835523bc1c919fe9:MTUwMzYxMTEwNy4xNjQ3">
+    <input type="hidden" name="mc_signupsource" value="hosted">
+    <a href="#" data-dismiss="modal" class="btn btn-close" style="margin-left:10%">NO GRACIAS</a>
+</form>
+</center>
+           
+         </div>
+
+       </div>
+     </div>
+</div>
 
 <!--Part 1-->
    <section>
@@ -75,14 +136,14 @@
          <div>
             <div class="caja1">
                <br>
-               <img src="imagenes/vianney.jpg" style="width:100%" class="img"></div> 
+               <img src="imagenes/cama1.png" style="width:65%" class="img"></div> 
          </div>
             <div class="caja2">
              <div class="contenido">
                <text class="titles">
-               <strong>
+               <i>
                LUCE TU HOGAR CON ELEGANCIA
-             </strong>
+             </i>
             </text>
                <br>
                <text class="titles2">
@@ -102,39 +163,27 @@
             </div>
          </div>
 
+
          </div>
       </article>
    </section>
    <!--Part1 end-->
-
    <!--Part 2-->
    <section class="contenido2">
       <article>
          <div class="caja3">
              <text class="titles">
-               ENCUENTRA EL DISEÑO PERFECTO PARA TU HOGAR
+              <br>
+               <i>ENCUENTRA EL DISEÑO PERFECTO PARA TU HOGAR</i>
             </text>
                <P>
                   <button class="form-btn semibold"><a href="https://www.blancorama.com/catalog/">VER CATÁLOGOS</a></button>
-            </div>
-            <div>
-               <br>
-               <img src="imagenes/background.jpg" style="width:100%"></div> 
                   <br>
+                  <br>
+                  <img src="imagenes/background2.png" style="width:100%"></div>
+
+            </div>
                   
-                  <div class="formulario" style="width:60%; display:inline-block;">
-                    <form action="https://facebook.us15.list-manage.com/subscribe/post" method="POST">
-    <input type="hidden" name="u" value="7138a58f3331cbc4ac586fc43">
-    <input type="hidden" name="id" value="9637fff923">
-                            <input type="text" name="MERGE0" id="MERGE0" required="required" class="form" placeholder="Ingresa tu correo para recibir más noticias"/>
-                         <button type="submit" value="Subscribe to list" name="submit" id="mc-embedded-subscribe" class="form-btn semibold">NOTIFICARME</button>
-                        </div>
-                        <div class="clear"></div>
-                    </form>
-
-                    <br>
-                    <br>
-
          </div>
       </article>
    </section>
